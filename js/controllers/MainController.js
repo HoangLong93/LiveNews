326245f9c4a2cdf00836ee4c2d57ade993ad73f7
Hoang Long Dinh <hoanglong0930@gmail.com>
2016-12-11 16:09:22 +0200

    change source

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,5 +1,22 @@
-app.controller('MainController', ['$scope', 'getData', function($scope, getData) {
-  getData.success(function(data) {
-    $scope.data = data;
-  });
+app.controller('MainController', ['$scope', 'getData','$interval', function($scope, getData, $interval) {
+  $scope.sources = ['abc-news-au','bbc-news','bbc-sport','bloomberg','business-insider','cnn','daily-mail','espn','fox-sports','independent','national-geographic','the-new-york-times','the-washington-post','time'];
+  $scope.selectSource = 'bbc-news';
+
+  //Handle function change source
+  $scope.changeSource = function(selectSource){
+      getData.getDataFromSource(selectSource).success(function(data) {
+        $scope.data = data;
+      });
+  }
+  $scope.changeSource('bbc-news')
+  
+  // $scope.intervalFunction = function(){
+  //   $interval(function() {
+      
+      
+  //   }, 10000)
+  // };
+  
+  // // Kick off the interval
+  // $scope.intervalFunction();
 }]);
--- js/services/getData.js
+++ js/services/getData.js
@@ -1,9 +1,14 @@
 app.factory('getData', ['$http', function($http) { 
-  return $http.get('https://newsapi.org/v1/articles?source=bbc-sport&sortBy=top&apiKey=83df7a3878c146b1ae6072c8742192f2') 
+  var getData = {};
+  getData.getDataFromSource = function(selectSource){
+      
+  return $http.get('https://newsapi.org/v1/articles?source=' + selectSource +'&sortBy=top&apiKey=83df7a3878c146b1ae6072c8742192f2') 
             .success(function(data) { 
               return data; 
             }) 
             .error(function(err) { 
               return err; 
             }); 
+  }
+  return getData;
 }]);
